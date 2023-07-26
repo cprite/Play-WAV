1be0aacfa37254cdb4ebe115a1ff5b5de4cd8e18
nikolay <98195072+cprite@users.noreply.github.com>
2023-07-26 17:21:27 +0100

    Add files via upload

---
+++ play_wav.py
@@ -0,0 +1,22 @@
+# SPDX-FileCopyrightText: 2021 Kattni Rembor for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+"""
+CircuitPython I2S WAV file playback.
+Plays a WAV file once.
+"""
+import audiocore
+import board
+import audiobusio
+
+audio = audiobusio.I2SOut(board.GP0, board.GP1, board.GP2)
+
+wave_file = open("felicita2.wav", "rb")
+wav = audiocore.WaveFile(wave_file)
+
+print("Playing wav file!")
+audio.play(wav)
+while audio.playing:
+    pass
+print("Done!")
